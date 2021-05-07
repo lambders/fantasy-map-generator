50ce9d576f3539c4376bfae464270c03d50464ba
Amanda Vu <amandavu94@gmail.com>
2021-05-07 14:43:38 -0400

    about face! incorporating some improvements from other gan project

--- main.py
+++ main.py
@@ -1,90 +1,117 @@
-import argparse
-from train import Trainer
-from sample import Sampler 
+import os 
+import argparse 
 
-# ARGPARSER 
-parser = argparse.ArgumentParser(description="fantasy-map-generator options")
-parser.add_argument("--mode",
-                    type=str,
-                    help="run the network in train or sample mode",
-                    default="train",
-                    choices=["train", "sample"])
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as tvt 
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-# DIRECTORY options
-parser.add_argument("--data_dir",
-                    type=str,
-                    help="path to the training data",
-                    default="dataset")
-parser.add_argument("--save_dir",
-                    type=str,
-                    help="path to save model and logs",
-                    default="logs")
-parser.add_argument("--exp_name",
-                    type=str,
-                    help="name of experiment, will be a subfolder of save_dir",
-                    default="exp1")
-parser.add_argument("--weights_dir",
-                    type=str,
-                    help="name of model to load")
+def train(data_dir: str,
+          load_dir: str, save_dir: str, 
+          num_epochs: int, batch_size: int, lr: float, 
+          use_gpu: bool):
+    # Dataloader
+    dataset = datasets.ImageFolder(
+        root = data_dir,
+        transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
+            transforms.Resize(128),
+            transforms.ToTensor(),
+        ]))
 
-# TRAINING options
-parser.add_argument("--batch_size",
-                    type=int,
-                    help="batch size",
-                    default=16)
-parser.add_argument("--learning_rate",
-                    type=float,
-                    help="learning rate",
-                    default=2e-4)
-parser.add_argument("--num_epochs",
-                    type=int,
-                    help="number of epochs",
-                    default=100)
-parser.add_argument("--gradient_penalty_weight",
-                    type=float,
-                    help="gradient penalty weight",
-                    default=10)
-parser.add_argument("--num_workers",
-                    type=int,
-                    help="number of workers for the data loading",
-                    default=4)
-parser.add_argument("--no_cuda",
-                    help="if set, train on cpu instead of gpu",
-                    action="store_true")
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8, drop_last=True)
 
-# NETWORK options
-parser.add_argument("--im_size",
-                    type=int,
-                    help="size of image in pixels",
-                    default=128)
-parser.add_argument("--latent_size",
-                    type=int,
-                    help="size of image in pixels",
-                    default=256)
-parser.add_argument("--num_blocks",
-                    type=int,
-                    help="number of convolutional blocks",
-                    default=4)
+    # Writer
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    writer = SummaryWriter(save_dir)
 
-# LOGGING options
-parser.add_argument("--log_frequency",
-                    type=int,
-                    help="number of batches between each tensorboard log",
-                    default=10)
-parser.add_argument("--save_frequency",
-                    type=int,
-                    help="number of epochs between each save",
-                    default=1)
+    # Network
+    from model import FantasyMapGAN
+    map_gan = FantasyMapGAN(lr, batch_size)
+    if use_gpu:
+        map_gan = map_gan.cuda()
+
+    # Train
+    step = 0
+    for epoch in range(num_epochs):
+        for i, (x, y) in enumerate(loader):
+            if use_gpu:
+                x = x.cuda()
+            loss_dict = map_gan.forward(x)
+            step += 1
+            print("Step % i" % step, loss_dict)
+
+            # Log
+            if i % 100 == 0:
+                for (k,v) in loss_dict.items():
+                    writer.add_scalar(k, v.item(), step)
+        
+        # Save
+        if epoch % 20 == 0:
+            map_gan.save(save_dir, epoch)
+            print("Save new model at epoch %i." % epoch)
+
+            # Log sample image
+            with torch.no_grad():
+                n_sample = 3
+                x_gen = map_gan.sample(n_sample)
+                gen_image = make_grid(x_gen, nrow=n_sample, normalize=True)
+                writer.add_image('image/gen', gen_image, epoch)
+
+                x_recon = map_gan.reconstruct(x[:n_sample])
+                recon_image = make_grid(x_recon, nrow=n_sample, normalize=True)
+                writer.add_image('image/recon', recon_image, epoch)
+
+                orig_image = make_grid(x[:n_sample], nrow=n_sample, normalize=True)
+                writer.add_image('image/orig', orig_image, epoch)
 
 
+def test(root_dir, csv_file, load_dir):
+    return
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="fantasy-map-generator options")
+    parser.add_argument("--mode",
+                        type=str,
+                        help="run the network in train or sample mode",
+                        default="train",
+                        choices=["train", "sample"])
+
+    # DIRECTORY options
+    parser.add_argument("--save_dir",
+                        type=str,
+                        help="path to save model and logs",
+                        default="logs")
+    parser.add_argument("--load_dir",
+                        type=str,
+                        help="name of model to load")
+
+    # TRAINING options
+    parser.add_argument("--data_dir",
+                        type=str,
+                        help="path to the training data",
+                        default="./maps")
+    parser.add_argument("--batch_size",
+                        type=int,
+                        help="batch size",
+                        default=64)
+    parser.add_argument("--learning_rate",
+                        type=float,
+                        help="learning rate",
+                        default=2e-4)
+    parser.add_argument("--num_epochs",
+                        type=int,
+                        help="number of epochs",
+                        default=50000)
+    parser.add_argument("--use_gpu",
+                        help="if set, train on gpu instead of cpu",
+                        action="store_true")
+
     options = parser.parse_args()
-
     if options.mode == 'train':
-        trainer = Trainer(options)
-        trainer.train()
-
+        train(options.data_dir, options.load_dir, options.save_dir, options.num_epochs, options.batch_size, options.learning_rate, options.use_gpu)
     elif options.mode == 'sample':
-        sampler = Sampler(options)
-        sampler.sample()
+        sample()
--- model.py
+++ model.py
@@ -1,85 +1,443 @@
-import math
+'''
+Networks for the map generator.
+'''
+import os
+from glob import glob
 import torch
-from torch.nn import Conv2d, ConvTranspose2d, GroupNorm
-from torch.nn import LeakyReLU, Sigmoid, ReLU, Tanh
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.nn.utils import spectral_norm
 
 
-class Generator(torch.nn.Module):
-    def __init__(self, im_size, latent_size, num_blocks):
-        """
-        Generator class.
-        """
-        super(Generator, self).__init__()
 
-        def block(in_channels, out_channels, kernel_size, stride, padding):
-            return torch.nn.Sequential(
-                ConvTranspose2d(
-                    in_channels, out_channels, 
-                    kernel_size, stride, padding, bias=False
-                ),
-                ReLU()
-            )
+class FantasyMapGAN(nn.Module):
 
-        # Layers
-        # ------
-        layers = []
-        # First block
-        num_filters = min(im_size * 2**(num_blocks-1), im_size*8)
-        layers.append(block(latent_size, num_filters, 4, 1, 0))
+    def __init__(self, lr: float, batch_size: int, ngf: int = 64, ndf: int = 64, latent_dim: int = 100):
+        '''
+        Fantasy map generator. 
+        A combination of a conditional variational autoencoder (for stability and attribute control) and a GAN (for generation power).
 
-        # Middle blocks
-        for i in range(num_blocks - 1):
-            num_filters_ = num_filters
-            num_filters = min(im_size * 2** (num_blocks-2-i), latent_size*8)
-            layer = block(num_filters_, num_filters, 4, 2, 1)
-            layers.append(layer)
+        Parameters
+        ----------
+        lr: float
+            learning rate
+        ngf: int
+            number of base filters to be used in the generator (encoder and decoder networks)
+        ndf: int
+            number of base filters to be used in the discriminators (ImageDiscriminator and LatentDiscriminator)
+        latent_dim: int
+            size of latent dimension
+        isTrain: bool
+            set to True if we want to train the network
+        '''
+        super(FantasyMapGAN, self).__init__()
+        self.batch_size = batch_size
+        self.latent_dim = latent_dim 
 
-        # End block
-        layers.extend([
-            ConvTranspose2d(num_filters, 1, 4, 2, 1),
-            Tanh()
-        ])
+        # Networks
+        self.encoder = Encoder(ngf, latent_dim)
+        self.decoder = Decoder(ngf, latent_dim)
+        self.disc_image = DiscriminatorImage(ndf)
+        self.disc_latent = DiscriminatorLatent(ndf, latent_dim)
+
+        # Optimizers
+        self.opt_generator = torch.optim.Adam(list(self.encoder.parameters()) + list(self.decoder.parameters()), lr, betas=(0.5, 0.9))
+        self.opt_disc_image = torch.optim.Adam(self.disc_image.parameters(), lr/2, betas=(0.5, 0.9))
+        self.opt_disc_latent = torch.optim.Adam(self.disc_latent.parameters(), lr/2, betas=(0.5, 0.9))
+
+        # Losses 
+        self.real_label = torch.ones(batch_size).cuda()
+        self.fake_label = torch.zeros(batch_size).cuda()
     
-        self.layers = torch.nn.Sequential(*layers)
+    def forward(self, x: torch.Tensor):
 
-    def forward(self, x):
-        return self.layers(x)
+        # Discriminator loss
+        self.opt_disc_latent.zero_grad()
+        self.opt_disc_image.zero_grad()
 
+        z = torch.randn((self.batch_size, self.latent_dim), requires_grad=True)
+        z = z.cuda()
 
-class Discriminator(torch.nn.Module):
-    def __init__(self, im_size, latent_size, num_blocks):
-        """
-        Discriminator class, inspired by DCGAN.
-        """
-        super(Discriminator, self).__init__()
-        
-        def block(in_channels, out_channels, kernel_size, stride, padding):
-            return torch.nn.Sequential(
-                Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
-                GroupNorm(1, out_channels),
-                LeakyReLU(0.2)
-            )
+        with torch.no_grad():
+            x_hat = self.decoder(z)
+            z_hat = self.encoder(x)
+            x_tilde = self.decoder(z_hat)
+            z_tilde = self.encoder(x_hat)
 
-        # Layers
-        # --------
-        layers = []
-        # First block
-        num_filters = im_size
-        layers.extend([
-            Conv2d(1, num_filters, 4, 2, 1),
-            LeakyReLU(0.2)
-        ])
+        x_conf = self.disc_image(x)
+        x_hat_conf = self.disc_image(x_hat)
+        x_tilde_conf = self.disc_image(x_tilde)
+        z_conf = self.disc_latent(z)
+        z_hat_conf = self.disc_latent(z_hat)
+        z_tilde_conf = self.disc_latent(z_tilde)
 
-        # Middle blocks
-        for i in range(num_blocks - 1):
-            num_filters_ = num_filters
-            num_filters = min(im_size * 2**(i+1), im_size*8)
-            layers.append(block(num_filters_, num_filters, 4, 2, 1))
-        
-        layers.append(Conv2d(num_filters, 1, 4, 1, 0))
+        x_loss = 2 * F.binary_cross_entropy_with_logits(x_conf, self.real_label)
+        x_hat_loss = F.binary_cross_entropy_with_logits(x_hat_conf, self.fake_label)
+        x_tilde_loss = F.binary_cross_entropy_with_logits(x_tilde_conf, self.fake_label)
+        z_loss = 2 * F.binary_cross_entropy_with_logits(z_conf, self.real_label)
+        z_hat_loss = F.binary_cross_entropy_with_logits(z_hat_conf, self.fake_label)
+        z_tilde_loss = F.binary_cross_entropy_with_logits(z_tilde_conf, self.fake_label)
 
-        self.layers = torch.nn.Sequential(*layers)
+        disc_image_loss = (x_loss + x_hat_loss + x_tilde_loss) / 4
+        disc_latent_loss = (z_loss + z_hat_loss + z_tilde_loss) / 4
+        disc_loss = disc_image_loss + disc_latent_loss
+
+        disc_loss.backward()
+        self.opt_disc_latent.step()
+        self.opt_disc_image.step()
+
+        # Generator loss
+        self.opt_generator.zero_grad()
+
+        z2 = torch.randn((self.batch_size, self.latent_dim), requires_grad=True)
+        z2 = z2.cuda()
+
+        x_hat = self.decoder(z2)
+        z_hat = self.encoder(x)
+        x_tilde = self.decoder(z_hat)
+        z_tilde = self.encoder(x_hat)
+
+        x_hat_conf = self.disc_image(x_hat)
+        z_hat_conf = self.disc_latent(z_hat)
+        x_tilde_conf = self.disc_image(x_tilde)
+        z_tilde_conf = self.disc_latent(z_tilde)
+
+        x_hat_loss = F.binary_cross_entropy_with_logits(x_hat_conf, self.real_label)
+        z_hat_loss = F.binary_cross_entropy_with_logits(z_hat_conf, self.real_label)
+        x_tilde_loss = F.binary_cross_entropy_with_logits(x_tilde_conf, self.real_label)
+        z_tilde_loss = F.binary_cross_entropy_with_logits(z_tilde_conf, self.real_label)
+
+        x_recon_loss = F.l1_loss(x_tilde, x) 
+        x_loss = (x_hat_loss + x_tilde_loss) / 2 * 0.005
+        z_loss = (z_hat_loss + z_tilde_loss) / 2 * 0.1
+        gen_loss = x_loss + z_loss + x_recon_loss 
+
+        gen_loss.backward()
+        self.opt_generator.step()
+
+        # Return losses
+        loss_dict = {
+            'generator/im_recon_loss': x_recon_loss,
+            'generator/gan_loss': x_loss + z_loss,
+            'generator/total_loss': gen_loss,
+            'discriminator/latent_loss': disc_latent_loss,
+            'discriminator/image_loss': disc_image_loss
+        }
+        return loss_dict
+    
+    def sample(self, batch_size: int):
+        '''
+        Sample a map image.
+
+        Parameters
+        ----------
+        batch_size: int
+            batch size of generated sample
+        '''
+        with torch.no_grad():
+            z = torch.randn((batch_size, self.latent_dim))
+            z = z.cuda()
+            x = self.decoder(z)
+        return x 
     
 
-    def forward(self, x):
-        return self.layers(x)
+    def reconstruct(self, x: torch.Tensor):
+        z = self.encoder(x)
+        x_recon = self.decoder(z)
+        return x_recon
+
+    
+    def save(self, save_dir: str, epoch: int) -> None:
+        '''
+        Save network weights.
+
+        Parameters
+        ----------
+        save_dir: str
+            path to save network weights
+        epoch: int
+            current epoch
+        '''
+        # Save
+        torch.save(self.encoder.state_dict(), os.path.join(save_dir, "%i_enc.pth" % epoch))
+        torch.save(self.decoder.state_dict(), os.path.join(save_dir, "%i_dec.pth" % epoch))
+        torch.save(self.disc_image.state_dict(), os.path.join(save_dir, "%i_disc_image.pth" % epoch))
+        torch.save(self.disc_latent.state_dict(), os.path.join(save_dir, "%i_disc_latent.pth" % epoch))
+
+        # Only keep three most recent saves of our four models
+        num_keep = 3 * 4
+        fs = glob(os.path.join(save_dir, '*.pth'))
+        fs.sort(key=os.path.getmtime)
+        for f in fs[:-num_keep]:
+            os.remove(f)
+
+    
+    def load(self, load_dir: str) -> None:
+        '''
+        Load network weights.
+
+        Parameters
+        ----------
+        load_dir: str
+            path to load network weights from
+        '''
+        if not load_dir:
+            return
+
+        # Find most recent epoch
+        fs = glob(os.path.join(load_dir, '*.pth'))
+        fs.sort(key=os.path.getmtime)
+        epoch = int(fs[-1].split('_')[0])
+
+        # Load
+        self.encoder.load_state_dict(torch.load(os.path.join(load_dir, "%i_enc.pth" % epoch)))
+        self.decoder.load_state_dict(torch.load(os.path.join(load_dir, "%i_dec.pth" % epoch)))
+        self.disc_image.load_state_dict(torch.load(os.path.join(load_dir, "%i_disc_image.pth" % epoch)))
+        self.disc_latent.load_state_dict(torch.load(os.path.join(load_dir, "%i_disc_latent.pth" % epoch)))
+
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, num_filters: int = 64, latent_dim: int = 100):
+        '''
+        The encoder maps from the image space to the latent space.
+
+        Parameters
+        ----------
+        num_filters: int
+            base number of filters used, by default 32
+        latent_dim: int
+            size of latent dimension, by default 10
+        '''
+        super(Encoder,self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(1,num_filters,5,2,2),
+            nn.Dropout(p=0.3),
+            nn.LeakyReLU(),
+            nn.Conv2d(num_filters,2*num_filters,5,2,2),
+            nn.Dropout(p=0.3),
+            nn.LeakyReLU(),
+            nn.Conv2d(2*num_filters,4*num_filters,5,2,2),
+            nn.Dropout(p=0.3),
+            nn.LeakyReLU(),
+            nn.Conv2d(4*num_filters,8*num_filters,5,2,2),
+            nn.Dropout(p=0.3),
+            nn.LeakyReLU(),
+            nn.Conv2d(8*num_filters,8*num_filters,5,2,2),
+            nn.Dropout(p=0.3),
+            nn.LeakyReLU(),
+        )
+        self.fc = nn.Linear(8192,latent_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+        Forward pass of encoder.
+
+        Parameters
+        ----------
+        x: torch.Tensor
+            input image, a tensor of shape (?,3,h,w)
+        
+        Returns
+        -------
+        torch.Tensor: latent vector of shape (?, latent_dim)
+        '''
+        batch_size = x.shape[0]
+        conv = self.layers(x)
+        conv = conv.view(batch_size,-1)
+        out = self.fc(conv)
+        return out
+
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, num_filters: int = 64, latent_dim: int = 100, color_dim: int = 16):
+        '''
+        The decoder maps from the latent space to the image space.
+
+        Parameters
+        ----------
+        num_filters: int
+            base number of filters used, by default 32
+        latent_dim: int
+            size of latent dimension, by default 10
+        '''
+        super(Decoder,self).__init__()
+        self.num_filters = num_filters
+        self.color_dim = color_dim 
+
+        def color_picker(input_dim: int, output_dim: int):
+            '''
+            Create and choose from a color palette during map generation.
+            Helps with overall perceptual quality of network. 
+            '''
+            colorspace = nn.Sequential(
+                nn.Linear(input_dim,128,bias=True),
+                nn.BatchNorm1d(128),
+                nn.ReLU(True),
+                nn.Linear(128,64,bias=True),
+                nn.BatchNorm1d(64),
+                nn.ReLU(True),
+                nn.Linear(64,output_dim,bias=True),
+                nn.Tanh(),
+            )
+            return colorspace
+
+        self.fc = nn.Sequential(
+            nn.Linear(latent_dim, 16*num_filters),
+            nn.BatchNorm1d(16*num_filters),
+            nn.ReLU(True)
+        )
+
+        self.upconv= nn.Sequential( # 6 12 24 48 96
+            nn.Conv2d(16*num_filters,8*num_filters,3,1,1),
+            nn.BatchNorm2d(8*num_filters),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(8*num_filters,4*num_filters,3,1,1),
+            nn.BatchNorm2d(4*num_filters),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(4*num_filters,2*num_filters,3,1,1),
+            nn.BatchNorm2d(2*num_filters),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(2*num_filters,num_filters,3,1,1),
+            nn.BatchNorm2d(num_filters),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(num_filters,num_filters,3,1,1),
+            nn.BatchNorm2d(num_filters),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(num_filters,color_dim,3,1,1), 
+            nn.Softmax(),
+        )
+
+        self.color_picker = color_picker(16*num_filters, color_dim)
+        
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        '''
+        Forward pass of generator network.
+
+        Parameters
+        ----------
+        z: torch.Tensor
+            latent input tensor, random noise
+        
+        Returns
+        -------
+        torch.Tensor: generated images
+        '''
+        # Generate 16-channel intermediate image from latent vector
+        x = self.fc(z)
+        x_square = x.view(-1,16*self.num_filters,1,1)
+        x_square = F.upsample(x_square, scale_factor=4)
+        intermediate = self.upconv(x_square)
+
+        # Pick from color palette
+        r = self.color_picker(x)
+        r = r.view((-1, 16, 1, 1))
+        r = F.upsample(r, scale_factor=128)
+        r = intermediate * r
+        r = torch.sum(r, dim=1, keepdim=True) 
+
+        return r
+
+
+class DiscriminatorImage(nn.Module):
+
+    def __init__(self, num_filters: int = 64):
+        '''
+        Discriminator for generated/real images.
+
+        Parameters
+        ----------
+        num_filters: int 
+            base number of filters used, by default 32
+        '''
+        super(DiscriminatorImage,self).__init__()
+        self.num_filters = num_filters
+
+        self.layers = nn.Sequential(
+            spectral_norm(nn.Conv2d(1,num_filters,4,2,1)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Conv2d(num_filters,num_filters*2,4,2,1)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Conv2d(num_filters*2,num_filters*4,4,2,1)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Conv2d(num_filters*4,num_filters*8,4,2,1)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Conv2d(num_filters*8,num_filters*8,4,2,1)),
+            nn.LeakyReLU()
+        )
+
+        self.fc = nn.Sequential(
+            spectral_norm(nn.Linear(8*4*4*num_filters,1024)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Linear(1024,1))
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+        Forward pass of image discriminator.
+
+        Parameters
+        ----------
+        x: torch.Tensor
+            input image, a tensor of shape (?, 3, h, w)
+        
+        Returns
+        -------
+        torch.Tensor: real/fake activations, a vector of shape (?,)
+        '''
+        y = self.layers(x)
+        y = y.view(-1,8*4*4*self.num_filters)
+        out = self.fc(y)
+        return out.squeeze()
+
+
+class DiscriminatorLatent(nn.Module):
+
+    def __init__(self, num_filters: int = 64, latent_dim: int = 100):
+        '''
+        Discriminator for latent vectors.
+
+        Parameters
+        ----------
+        num_filters: int
+            base number of filters used, by default 32
+        latent_dim: int
+            size of latent dimension, by default 10
+        '''
+        super(DiscriminatorLatent,self).__init__()
+
+        self.layers = nn.Sequential(
+            spectral_norm(nn.Linear(latent_dim,num_filters*4)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Linear(num_filters*4,num_filters*2)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Linear(num_filters*2,num_filters)),
+            nn.LeakyReLU(),
+            spectral_norm(nn.Linear(num_filters,1))
+        )
+    
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        '''
+        Forward pass of latent discriminator.
+
+        Parameters
+        ----------
+        z: torch.Tensor
+            input latent, a tensor of shape (?, latent_dim)
+        
+        Returns
+        -------
+        torch.Tensor: real/fake activations, a vector of shape (?,)
+        '''
+        return self.layers(z).squeeze()
